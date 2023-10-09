309e0bdb71807530565f45ecb5b5f814076e971a
Vishnu P V <vishnupv00143@gmail.com>
2023-10-09 14:42:33 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Button from '@mui/material/Button';
+import NavBar from './components/NavBar/NavBar';
+
+function App() {
+  
+  return (
+    <>
+      <NavBar/>
+      <h1>Hello World</h1>
+      <Button variant="contained">Click Me</Button>
+    </>
+  )
+}
+
+export default App
